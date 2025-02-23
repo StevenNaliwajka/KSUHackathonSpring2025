8822e770a06df2282a3da5959ffc7572d831cbac
kaaat777 <114625453+kaaat777@users.noreply.github.com>
2025-02-22 21:42:26 -0500

    FIXED IT LETS GO

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
 from flask import render_template, request, redirect, url_for, session
 
+app.secret_key= "hello"
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
     data = request.form
@@ -9,8 +11,8 @@ def index():
         
         name = request.form.get('name')
         phone = request.form.get('phone')
-        weather_updates = request.form.get('weatherUpdates')
-        future_games = request.form.get('futureGames')
+        weather_updates = request.form.get('weatherUpdates') == "True"
+        future_games = request.form.get('futureGames') == "True"
 
         
         session["user"] = {
@@ -19,6 +21,7 @@ def index():
             "weather_updates": weather_updates,
             "future_games": future_games,
         }
+        print("User registered:", session["user"])
         return redirect(url_for("dashboard"))
 
     return render_template('index.html')
@@ -26,8 +29,10 @@ def index():
 @app.route('/dashboard')
 def dashboard():
     user = session.get("user")
-
+    print("User session data:", user)
     
+    if not user:
+        return redirect(url_for("index"))
     #place holder list to track users input for the picks until database is done
     user_picks = []
 
